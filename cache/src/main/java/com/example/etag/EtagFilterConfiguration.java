96b36e2ee4d7c99b096662312967baa16f56e912
beomWhale <cndqjacndqja@gmail.com>
2022-09-02 22:26:46 +0900

    feat: cache 학습 테스트 완료

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,26 @@
 package com.example.etag;
 
+import com.example.version.ResourceVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    private final ResourceVersion version;
+
+    public EtagFilterConfiguration(final ResourceVersion version) {
+        this.version = version;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
+                = new FilterRegistrationBean<>( new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/etag",  "/resources/" + version.getVersion() + "/js/index.js");
+        return filterRegistrationBean;
+    }
 }
